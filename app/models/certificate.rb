94e21effcbe9bc7dd90b587e4805efc04812efcd
Craig <craig.frey@hotmail.com>
2013-09-08 21:19:08 -0500

    updated model relationships

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -1,5 +1,5 @@
 class Certificate < ActiveRecord::Base
-
+  belongs_to :user
       has_attached_file :document
 
   validates_attachment :document, content_type: {content_type: ['application/pdf'] }, size: {less_than: 2.megabytes}
