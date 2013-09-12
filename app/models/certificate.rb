6983eba868d17a7fcb070f9ee999c57df8fc6b0a
Craig <craig.frey@hotmail.com>
2013-09-12 13:05:17 -0500

    fixed id in the certificate model

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -1,7 +1,7 @@
 class Certificate < ActiveRecord::Base
   belongs_to :user
       has_attached_file :document, {
-        :url => "/system/certificates/documents/000/000/021/original/:basename.pdf"
+        :url => "/system/certificates/documents/000/000/00:id/original/:basename.pdf"
       }
 
   validates_attachment :document, content_type: {content_type: ['application/pdf'] }, size: {less_than: 2.megabytes}
