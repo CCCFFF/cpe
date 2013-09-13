8bedfad48d137c53e29c78baf35742aea059f92a
Craig <craig.frey@hotmail.com>
2013-09-13 08:08:39 -0500

    push up for heroku

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -1,7 +1,7 @@
 class Certificate < ActiveRecord::Base
   belongs_to :user
       has_attached_file :document, {
-        :url => "/system/certificates/documents/000/000/00:id/original/:basename.pdf"
+        :url => "localhost:3000/system/certificates/documents/000/000/00:document/original/:basename.pdf"
       }
 
   validates_attachment :document, content_type: {content_type: ['application/pdf'] }, size: {less_than: 2.megabytes}
