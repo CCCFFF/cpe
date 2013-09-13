d21ea1b401e817f5fcccddc72c20c0ef3c9a4d99
Craig <craig.frey@hotmail.com>
2013-09-13 09:40:44 -0500

    changed certificate model from pdf to image

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -1,10 +1,9 @@
 class Certificate < ActiveRecord::Base
   belongs_to :user
-      has_attached_file :document, {
-        :url => "localhost:3000/system/certificates/documents/000/000/00:document/original/:basename.pdf"
-      }
 
-  validates_attachment :document, content_type: {content_type: ['application/pdf'] }, size: {less_than: 2.megabytes}
+      has_attached_file :document, :styles => { :large => "900x700>" }
+
+  validates_attachment :document, content_type: {content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] }, size: {less_than: 2.megabytes}
 
 
 
